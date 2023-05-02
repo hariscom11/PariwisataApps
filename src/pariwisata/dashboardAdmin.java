468f9fab00fe01cd5214e0c03a7a7899059fece9
E41222732_Muhammad Haris Suhud_B <harissuhud572@gmail.com>
2023-05-02 10:01:22 +0700

    layout update

--- src/pariwisata/dashboardAdmin.java
+++ src/pariwisata/dashboardAdmin.java
@@ -32,7 +32,7 @@ public class dashboardAdmin extends javax.swing.JFrame {
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
         getContentPane().setLayout(new org.netbeans.lib.awtextra.AbsoluteLayout());
 
-        jLabel1.setIcon(new javax.swing.ImageIcon("E:\\Semester 2\\project\\UI\\MacBook Pro 14_ - 11.png")); // NOI18N
+        jLabel1.setIcon(new javax.swing.ImageIcon(getClass().getResource("/image/MacBook Pro 14_ - 11.png"))); // NOI18N
         getContentPane().add(jLabel1, new org.netbeans.lib.awtextra.AbsoluteConstraints(0, 0, -1, -1));
 
         pack();
